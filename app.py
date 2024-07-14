7ef2dd026d31cae707dbec63bc29cb63476deeab
ImaneIK <imaneik0910@gmail.com>
2024-07-14 21:45:23 +0100

    deployment

--- app.py
+++ app.py
@@ -388,5 +388,5 @@ def export_pdf():
         return make_response(jsonify({"error": str(e)}), 500)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
